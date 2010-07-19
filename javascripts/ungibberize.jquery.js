a671a574abb26a5bdebb6e88c7182d27781f4c6f
Yuval Tobias <yuval@irgun.co.il>
2010-07-20 01:27:58 +0300

    merged both js files, and added a minified version

--- javascripts/ungibberize.jquery.js
+++ javascripts/ungibberize.jquery.js
@@ -108,3 +108,82 @@ jQuery.fn.extend({
         });
     }
 });
+
+this.ungibberize = (function() {
+    var hebrewToEnglishConversionTable = new Array();
+    var englishToHebrewConversionTable = new Array();
+
+    createConversionTables();
+
+    function createConversionTables() {
+      var hebrewChars  = ";/'קראטוןםפשדגכעיחלךף,זסבהנמצתץ."; 
+      var englishChars = "`qwertyuiopasdfghjkl;'zxcvbnm,./";
+
+      for( var i=0; i<hebrewChars.length; i++ ) {
+        hebrewToEnglishConversionTable[hebrewChars[i]]  = englishChars[i];
+        englishToHebrewConversionTable[englishChars[i]] = hebrewChars[i];
+      }
+    }
+
+    function convert( s, table ) {
+      var converted = "";
+      for(var i=0; i<s.length; i++) {
+        converted += table[s[i]] || s[i];
+      }
+      return converted;
+    }
+
+    function he2en( hebrewString ) {
+      return convert( hebrewString, hebrewToEnglishConversionTable);
+    } 
+
+    function en2he( englishString ) {
+      return convert( englishString, englishToHebrewConversionTable);
+    }
+    
+    function shouldDisplayUngibberize( s ) {
+        if( s.length < 4 ) return false;
+        
+        var hebrewChars  = 'אבגדהוזחטיכךלמםנןסעפףצץקרשת';
+        var englishChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var hebrewCharCount  = 0;
+        var englishCharCount = 0;
+        
+        for( i = 0; i < s.length; i++ ) {
+            if( hebrewChars.indexOf(s[i]) >= 0 ) {
+                hebrewCharCount++;
+            } else if( englishChars.indexOf(s[i]) >= 0 ) {
+                englishCharCount++;
+            }
+        }
+        
+        return hebrewCharCount < englishCharCount;
+    }
+
+    function calcOptimalImageSize( n ) {
+        var availableSizes = new Array(12, 16, 20, 32);
+
+        for( i = 0; i < availableSizes.length-1; i++ ) {
+            if( availableSizes[i] >= n ) {
+                return availableSizes[i];
+            }
+        }
+
+        return availableSizes.pop();
+    }
+
+    function getHeButtonFilename( height ) {
+        return "images/ungibberize-he-" + calcOptimalImageSize(height) + ".png"
+    }
+
+    function getUndoRTLButtonFilename( height ) {
+        return "images/ungibberize-rtlundo-" + calcOptimalImageSize(height) + ".png"
+    }
+
+    return {
+        en2he : en2he,
+        shouldDisplayUngibberize : shouldDisplayUngibberize,
+        getHeButtonFilename : getHeButtonFilename,
+        getUndoRTLButtonFilename : getUndoRTLButtonFilename
+    }
+})();
